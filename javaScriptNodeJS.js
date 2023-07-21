edd08a1a13c4ccd2b4e99086a4f8d391ff0941ab
DiogoAsenjo <diogoasenjo@outlook.com>
2023-07-21 12:18:13 -0300

    JavaScript e Node.js

---
+++ javaScriptNodeJS.js
@@ -0,0 +1,8 @@
+//Tipagem dinâmica
+//O tipo da variável pode ser alterada durante o programa, não é necessário informar o tipo da variável;
+let variavel = 33;
+console.log(typeof(variavel)); //Number
+variavel = `string`;
+console.log(typeof(variavel)); //String
+variavel = true;
+console.log(typeof(variavel)); //Boolean
