2e153f87da4d9d3950a1667597d9588d7e455aae
DiogoAsenjo <diogoasenjo@outlook.com>
2023-07-24 10:38:28 -0300

    Desestruturando listas

--- lacosRepeticao.js
+++ lacosRepeticao.js
@@ -4,9 +4,11 @@ let notas = [10, 8.5, 9, 5, 1, 0, 6.5];
 let alunosENotas = [alunos, notas];
 
 function exibeNomeENota(nome) {
-    if(alunosENotas[0].includes(nome)) { //Includes procura em todo o array se há um elemento correspondente ao parâmetro passado
-        const indice = alunosENotas[0].indexOf(nome); //indexOf diz qual o índice do parâmetro passado
-        console.log(`${nome} está cadastrado e tirou nota ${alunosENotas[1][indice]}`);
+    //Desestruturando lista. Quando temos arrays de duas dimensões, o código poda ficar confuso tendo que colocar várias índices, por isso podemos desestruturar da seguinte forma;
+    const [alunos, notas] = alunosENotas;
+    if(alunos.includes(nome)) { //Includes procura em todo o array se há um elemento correspondente ao parâmetro passado
+        const indice = alunos.indexOf(nome); //indexOf diz qual o índice do parâmetro passado
+        console.log(`${nome} está cadastrado e tirou nota ${notas[indice]}`);
     } else {
         console.log(`Aluno não encontrado`);
     }
