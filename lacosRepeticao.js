bc2cc7f716dba0b5b8ef4d00e0456a22ec1faa45
DiogoAsenjo <diogoasenjo@outlook.com>
2023-07-24 10:46:55 -0300

    Laços de repetição - for

--- lacosRepeticao.js
+++ lacosRepeticao.js
@@ -5,7 +5,7 @@ let alunosENotas = [alunos, notas];
 
 function exibeNomeENota(nome) {
     //Desestruturando lista. Quando temos arrays de duas dimensões, o código poda ficar confuso tendo que colocar várias índices, por isso podemos desestruturar da seguinte forma;
-    const [alunos, notas] = alunosENotas;
+    const [alunos, notas] = alunosENotas; //Aqui, cada array dentro da alunosENotas está ganhando um nome como se fosse um único array, por isso se chama desestruturar;
     if(alunos.includes(nome)) { //Includes procura em todo o array se há um elemento correspondente ao parâmetro passado
         const indice = alunos.indexOf(nome); //indexOf diz qual o índice do parâmetro passado
         console.log(`${nome} está cadastrado e tirou nota ${notas[indice]}`);
@@ -15,3 +15,14 @@ function exibeNomeENota(nome) {
 }
 
 exibeNomeENota(`Diogo`);
+
+//Laços de repetição
+let numeros = [];
+let num = 100;
+
+for(i = 0; i < 6; i++) {
+    numeros.push(num);
+    num = num + 100;
+}
+
+console.log(numeros);
