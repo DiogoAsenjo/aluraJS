343b476ae3abb2a41dc0e517a133e79a777beec5
DiogoAsenjo <diogoasenjo@outlook.com>
2023-07-24 16:46:41 -0300

    Introdução a objetos

---
+++ Objetos/introducao-objetos.js
@@ -0,0 +1,10 @@
+//O que são objetos? Em JS, um objeto é uma entidade independente com propriedades e tipos;
+
+const cliente = { //Aqui temos o objeto cliente, suas propriedades são nome, e-mail, etc, dessa forma, ao invés de acessar os dados por índice como num array, podemos dar nomes a essas propriedades;
+    nome: `Diogo`,
+    mail: `diogoasenjo@outlook.com`,
+    DN: `29/03/1996`,
+    cpf: `22745099809`,
+}
+
+console.log(`Bem vindo ao nosso site ${cliente.nome}!`);
