b9ebcf0f0029b282bfb37e213f738e42c3dbe129
DiogoAsenjo <diogoasenjo@outlook.com>
2023-07-25 10:42:59 -0300

    Adicionando e alterando propriedades dos objetos

--- Objetos/introducao-objetos.js
+++ Objetos/introducao-objetos.js
@@ -10,3 +10,12 @@ const cliente = { //Aqui temos o objeto cliente, suas propriedades são nome, e-
 console.log(`Bem vindo ao nosso site ${cliente.nome}!`);
 console.log(`Bem vindo ao nosso site ${cliente["nome"]}!`); //Ao invés do ."propriedade", podemos utilizar os colchetes;
 
+//Manipulando dados dos objetos
+
+cliente.telefone = `13991908026`; //Adicionando propriedade;
+cliente.cpf = `44499988803`; //Alterando propriedade existente;
+
+//Obs.: as variáveis constantes, não pode ser reatribuídas, mas podem ser alteradas;
+
+console.log(cliente.telefone);
+console.log(cliente);
