60e6f72e480119f0de7ddca6673430a6ce39d455
DiogoAsenjo <diogoasenjo@outlook.com>
2023-07-26 09:59:28 -0300

    Função como propriedade de um objeto

--- Objetos/manipulandoObjetos.js
+++ Objetos/manipulandoObjetos.js
@@ -20,7 +20,7 @@ cliente.endereço = [{
 },
 ]
 
-//E obviamente, como se trata de um array, podemos usar métodos de array para adicionar novos elementos;
+//E obviamente, como se trata de um array, podemos usar métodos de array para adicionar novos elementos, ou para qualquer outra coisa, filter(), map(), etc.;
 cliente.endereço.push({
     rua: `Visconde de Rio Branco`,
     numero: `02`,
@@ -28,4 +28,18 @@ cliente.endereço.push({
     complemento: `6º andar`,
 })
 
-console.log(cliente);
+//console.log(cliente);
+
+
+//Funções como propriedade de um objeto;
+cliente.saldo = 300; //Adicionando propriedade saldo ao objeto cliente;
+cliente.efetuaPagamento = function(valor) { //Aqui estamos adicioando a função como propriedade, ela irá verificar se é possível ou não fazer a compra;
+    if(valor > this.saldo) { //Dá pra usar o this.propriedade, para utilizar o o valor dentro do objeto;
+        console.log(`Saldo insuficiente`);
+    } else {
+        this.saldo -= valor;
+        console.log(`Pagamento realizado! Saldo atual: ${this.saldo}`);
+    }
+};
+
+cliente.efetuaPagamento(150);
