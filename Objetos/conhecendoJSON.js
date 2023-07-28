8eb37f491a3654210011702d9fd8d429978727b5
DiogoAsenjo <diogoasenjo@outlook.com>
2023-07-28 09:01:06 -0300

    Lendo o arquivo JSON

--- Objetos/conhecendoJSON.js
+++ Objetos/conhecendoJSON.js
@@ -1,5 +1,6 @@
 // JSON - JavaScript Object Notation, é uma forma de salvar objetos em JS;
 
+//Aqui como escrevemos um objeto em JS;
 const cliente = {
     nome: `Diogo`,
     mail: `diogoasenjo@outlook.com`,
@@ -11,7 +12,31 @@ const cliente = {
         numero: `28`,
         apartamento: true,
         complemento: `Apt 44`,
-    }
+    },
 }
 
-console.log(cliente);
+//Aqui como ele deve ser escrito nos arquivos JSON, também está no arquivo cliente.json;
+/*
+//Obs1.: não declaramos a variável;
+{
+    //Obs2.: as propriedades estão entre aspas duplas "";
+    "nome": `Diogo`,
+    "mail": `diogoasenjo@outlook.com`,
+    "DN": `29/03/1996`,
+    "cpf": `22745099809`,
+    "telefones": [`13991908026`, `1332881668`],
+    "enderecos":{
+        "rua": `Colômbia`,
+        "numero": `28`,
+        "apartamento": true,
+        "complemento": `Apt 44`,
+    } //Obs3.: não tem vírgula após a última propriedade;
+}
+Obs4.: Não permite funções, não permite comentários e suporta apenas tipos primitivos;
+*/
+
+//Lendo um arquivo JSON
+
+const dados = require("./cliente.json");
+console.log(dados);
+console.log(typeof dados);
