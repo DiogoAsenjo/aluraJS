34a260282afc17a178d03a480de95b715daffbc9
DiogoAsenjo <diogoasenjo@outlook.com>
2023-07-28 08:55:23 -0300

    Conhecendo e criando o primeiro arquivo JSON

---
+++ Objetos/conhecendoJSON.js
@@ -0,0 +1,17 @@
+// JSON - JavaScript Object Notation, é uma forma de salvar objetos em JS;
+
+const cliente = {
+    nome: `Diogo`,
+    mail: `diogoasenjo@outlook.com`,
+    DN: `29/03/1996`,
+    cpf: `22745099809`,
+    telefones: [`13991908026`, `1332881668`],
+    enderecos:{
+        rua: `Colômbia`,
+        numero: `28`,
+        apartamento: true,
+        complemento: `Apt 44`,
+    }
+}
+
+console.log(cliente);
