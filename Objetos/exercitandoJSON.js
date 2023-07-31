1a210782652391378fc8537f050eeabf25b4bab8
DiogoAsenjo <diogoasenjo@outlook.com>
2023-07-31 08:25:58 -0300

    Filtrando dados de um objeto

--- Objetos/exercitandoJSON.js
+++ Objetos/exercitandoJSON.js
@@ -1,6 +1,7 @@
 //Desafio 01. Encontrando clientes dentro do arquivo JSON
 
 const dadosClientes = require("./exercitandoJSONClientes.json");
+//Em dados clientes teremos um array com diversos objetos, cada um representa um cliente.
 
 function encontrar(lista, chave, valor) {
     return lista.find((item) => item[chave].includes(valor));
@@ -11,7 +12,14 @@ const encontrado2 = encontrar(dadosClientes, "telefone", "19918820860");
 
 //console.log(encontrado1);
 
-//Desafio 02. Filtrando um objeto
+//Desafio 02. Filtrando um objeto. Quais clientes possuem apartamento porém não preencheram o campo complemento.
 
-const clientesComApartamento = dadosClientes.filter((item) => item.endereco.apartamento === true);
-console.log(clientesComApartamento);
+function filtrarApartamentoSemComplemento(dadosClientes) {
+    return dadosClientes.filter((cliente) => {
+        return (cliente.endereco.apartamento && !cliente.endereco.hasOwnProperty("complemento"));
+    })
+}
+
+const apartamentosSemcomplemento = filtrarApartamentoSemComplemento(dadosClientes);
+
+console.log(apartamentosSemcomplemento);
